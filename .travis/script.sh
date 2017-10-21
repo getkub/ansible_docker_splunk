cb445e912c8ca6a93c795d3121d6e30d7fa527d7
getkub <getkub@users.noreply.github.com>
2017-10-21 22:13:16 +0100

    Create script.sh

---
+++ .travis/script.sh
@@ -0,0 +1,11 @@
+set -ev
+set -o pipefail
+
+# Grab the parent (root) directory.
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+ME=`basename "$0"`
+
+echo ${ME} `date`
+
+cd ${DIR}
+make build
