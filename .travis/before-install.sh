ba5db292594991833d805ffc6568f80ea9bf9208
getkub <getkub@users.noreply.github.com>
2017-10-21 22:11:43 +0100

    Create before-install.sh

---
+++ .travis/before-install.sh
@@ -0,0 +1,8 @@
+# Exit on first error, print all commands.
+set -ev
+set -o pipefail
+
+# Grab the parent (root) directory.
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+
+echo "before-install DIR= $DIR "
