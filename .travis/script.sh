4061e7f24f2fd4023d35882ba1b8d7b1864aa635
getkub <getkub@users.noreply.github.com>
2017-10-24 11:56:59 +0100

    Update script.sh

--- .travis/script.sh
+++ .travis/script.sh
@@ -5,8 +5,8 @@ set -o pipefail
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 ME=`basename "$0"`
 
-echo ${ME} `date`
-
 cd ${DIR}
+echo ${ME} `date`  PWD=${DIR}
+
 # make clean
 make build
