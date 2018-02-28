6bcf0a2a7d402fd7a4e34eaa56152e8a66312339
getkub <kinbsk@gmail.com>
2018-02-28 19:33:28 +0000

    Updated vars and added mac specific python

---
+++ ansible/roles/build_splunk_apps/files/dev/MY_all_env_specific/bin/splunk_set_mac.py
@@ -0,0 +1,15 @@
+
+# Append OPTIMISTIC_ABOUT_FILE_LOCKING in $SPLUNK_HOME/etc/splunk-launch.conf
+
+import os
+
+sFile = '/opt/splunk/etc/splunk-launch.conf'
+
+if os.path.exists(sFile):
+    append_write = 'a' # append if already exists
+else:
+    append_write = 'w' # make a new file if not
+
+sFileObj = open(sFile,append_write)
+sFileObj.write("\nOPTIMISTIC_ABOUT_FILE_LOCKING = 1\n")
+sFileObj.close()
