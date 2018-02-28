b7c7f19f57e876b6bc37d5d8b37054004050f928
getkub <kinbsk@gmail.com>
2018-02-28 23:31:50 +0000

    Updated various apps to work with MAC

--- ansible/roles/build_splunk_apps/files/dev/MY_all_env_specific/bin/splunk_set_mac.py
+++ ansible/roles/build_splunk_apps/files/dev/MY_all_env_specific/bin/splunk_set_mac.py
@@ -13,3 +13,13 @@ else:
 sFileObj = open(sFile,append_write)
 sFileObj.write("\nOPTIMISTIC_ABOUT_FILE_LOCKING = 1\n")
 sFileObj.close()
+
+sFile2 = '/opt/splunk/etc/sample.temp'
+if os.path.exists(sFile2):
+    append_write = 'a' # append if already exists
+else:
+    append_write = 'w' # make a new file if not
+
+sFileObj2 = open(sFile2,append_write)
+sFileObj2.write("\nTest Writing\n")
+sFileObj2.close()
