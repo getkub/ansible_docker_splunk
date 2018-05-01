ac885c52f519f5174cf55ae99c49689abf629830
getkub <kinbsk@gmail.com>
2018-05-02 00:33:39 +0100

    Updated travis lines

--- .travis/script.sh
+++ .travis/script.sh
@@ -6,7 +6,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 ME=`basename "$0"`
 
 cd ${DIR}/ansible
-echo ${ME} `date`  PWD=${DIR}
+echo `date +%FT%T` PWD=`pwd`
 
 ansible-playbook -i hosts -c local site.yml
-
